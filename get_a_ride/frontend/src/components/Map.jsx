470243226c357da42069f7cb19981a89fb39088d
jafte <mostfaosi26@gmail.com>
2026-05-19 11:55:54 +0300

    Update Map

--- get_a_ride/frontend/src/components/Map.jsx
+++ get_a_ride/frontend/src/components/Map.jsx
@@ -3,6 +3,17 @@ import { useMapEvents } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import { useState } from 'react';
 import './Map.css';
+import L from 'leaflet';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+    iconUrl: markerIcon,
+    iconRetinaUrl: markerIcon2x,
+    shadowUrl: markerShadow,
+});
 
 function LocationPicker({ pointA, pointB, setPointA, setPointB }) {
     useMapEvents({
